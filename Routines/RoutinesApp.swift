b8c84281ba5a9bc785dc068d8b58eb3b602f2d6f
Indy <indymcginnis@gmail.com>
2022-10-19 17:38:18 -0700

    Initial Commit

---
+++ Routines/RoutinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutinesApp.swift
+//  Routines
+//
+//  Created by Indy McGinnis on 10/19/22.
+//
+
+import SwiftUI
+
+@main
+struct RoutinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
